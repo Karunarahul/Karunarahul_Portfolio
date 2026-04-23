78bff2874093ed3ed8a40819d82841c8df742fde
MKRahul <karunarahul8885@gmail.com>
2026-04-24 02:01:56 +0530

    chore: fix base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Karunarahul_Portfolio/', // GitHub Pages base path
   plugins: [react()],
 })
